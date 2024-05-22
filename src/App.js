4e246961a551aa18449fea6bbb9a201b86ba64ca
gualin <iiwwpr1@gmail.com>
2024-05-22 18:45:21 +0800

    gogo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        gap: "10px",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          window.location.href = "https://www.google.com";
+        }}
+      >
+        window.location.href
+      </button>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          window.location.assign("https://www.google.com");
+        }}
+      >
+        {" "}
+        window.location.assign
+      </button>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          window.open("https://www.google.com", "_blank");
+        }}
+      >
+        {" "}
+        window.open("https://www.google.com","_blank");
+      </button>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          window.open("https://www.google.com", "_system", "location=yes");
+        }}
+      >
+        {" "}
+        window.open("https://www.google.com", "_system", "location=yes");
+      </button>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          window.open("https://www.google.com", "_self");
+        }}
+      >
+        {" "}
+        window.open("https://www.google.com", "_self", );
+      </button>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          window.open("https://www.google.com", "_system");
+        }}
+      >
+        {" "}
+        window.open("https://www.google.com", "_system");
+      </button>
     </div>
   );
 }
